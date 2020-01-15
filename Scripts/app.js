9b4b0d9425064099fcc403d150b84123ec1ceb3b
JarodColley <47607281+JarodColley@users.noreply.github.com>
2020-01-15 13:57:29 -0500

    Changed products to projects in nav bar

---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+"user strict";
+
+// IIFE - immediatly invoked function expression
+// means -> anonymous self-executing function
+(function(){
+
+    function Start()
+    {
+    let projects = document.getElementById("products");
+    projects.firstChild.lastChild = "Projects";
+    console.log(projects);
+    }
+    window.addEventListener("load", Start);
+
+})();
