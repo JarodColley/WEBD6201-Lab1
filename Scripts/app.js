b90fe3bffaafd1284fd1e96a9ef704fdb4884b68
JarodColley <47607281+JarodColley@users.noreply.github.com>
2020-01-21 12:08:20 -0500

    added the Human Resources to the nav bar

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,9 +9,22 @@
 
     function Start()
     {
-    let projects = document.getElementById("products");
+    let projects = document.getElementById("Products");
     projects.firstChild.lastChild.textContent = " Projects";
     console.log(projects);
+
+    //putting the human resorces on the nav bar 
+    let humanResources = projects.cloneNode(true);
+    let contact = document.getElementById("Contact Us");
+    //inserts the node into nav bar
+    contact.parentNode.insertBefore(humanResources, contact);
+    //sets the text to Human resources
+    humanResources.firstChild.lastChild.textContent = " Human Resources";
+    //changes the icon
+    humanResources.firstChild.firstChild.setAttribute("class","fas fa-user-alt")
+    //changes id so it is not the same as the one that was clonned
+    humanResources.setAttribute("id", "Human Resources");
+
     }
     window.addEventListener("load", Start);
 
